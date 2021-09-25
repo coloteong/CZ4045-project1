e772009f38ae4a1d5649041a846c55a256d2a42d
Aurelio Jethro P <jethro4869@gmail.com>
2021-09-25 12:14:57 +0800

    added trial notebook to run it through the data, everything seems to worl

---
+++ indicative_adjphr/get_adj_phrases.py
@@ -0,0 +1,58 @@
+import spacy
+nlp = spacy.load("en_core_web_sm")
+
+def get_adjective_phrases(text) -> list:
+
+    def get_to_object(token) -> int:
+        for child in token.children:
+            if (child.pos_ in ['NOUN', 'PRON', 'PROPN']) and (child.dep_ == 'pobj') :
+                subtoken = list(child.children)
+                subtoken = [token for token in subtoken if token.dep_ == 'prep']
+                if subtoken:
+                    return get_to_object(subtoken[0])
+                else:
+                    return get_to_object(child)
+
+    doc = nlp(text)
+    phrases = []
+    for token in doc:
+        phrase = ''
+        """
+        An adjective has the POS - ADJ and it can be the root word in a sentence (rare),
+        it is more often in the form of `acomp` or `amod`, we immediately add this to our phrase
+        since this is the main constituent of the adjective phrase
+        """
+        if (token.pos_ == 'ADJ') and (token.dep_ in ['ROOT', 'acomp', 'amod']):
+            phrase += token.text
+            adjective_position = token.i
+            for subtoken in token.children:
+                # first rule: if there is an adverb that modifies the adjective
+                # we add it to the phrase in front of the adjective
+                if (subtoken.pos_ == 'ADV') and (subtoken.dep_ == 'advmod'):
+                    phrase = subtoken.text + ' ' + phrase
+                # second rule: if there is a preposition - indicating that
+                # there is an object that gives us more info about the prep
+                # we add all tokens up until the noun
+                if (subtoken.pos_ in ['ADP', 'SCONJ']) and (subtoken.dep_ == 'prep'):
+                    try:
+                        noun_position = get_to_object(subtoken)
+                        for i in range(adjective_position + 1, noun_position + 1):
+                            phrase += ' ' + doc[i].text
+                    except TypeError:
+                        pass
+        # need to fix so that it does not get random adverbs and adjectives
+        """elif (token.pos_ == 'AUX') and (token.dep_ == 'ROOT'):
+            for subtoken in token.children:
+                if subtoken.dep_ == 'advmod':
+                    phrase += subtoken.text + ' '
+                if subtoken.dep_ == 'acomp':
+                    phrase += subtoken.text + ' '"""
+        # since it is a phrase, it needs to have more than one word
+        # i.e. a lone adjective does not constitute an adjective phrase
+        if  len(phrase.split()) > 1:
+            phrases.append(phrase)
+    return phrases
+
+
+if __name__ == '__main__':
+    get_adjective_phrases("I guess it's classified as fast food since the whole process is similar to what you'd experience at subway but the taste is far beyond your typical tofu wrap.")
