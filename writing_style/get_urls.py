35da7360d15191ef8d652cec2a7799a0e4bd7c08
claudia <61492986+coloteong@users.noreply.github.com>
2021-09-28 19:14:42 +0800

    my bad habits lead to late nights ending alone
    did some new stuff, if effort was 10/10 the results were 4/10

--- writing_style/get_urls.py
+++ writing_style/get_urls.py
@@ -50,3 +50,45 @@ def getHWZurl():
         break
     print("Found URL: " + url)
     return hwz_text
+
+def getCNAurl():
+    url = 'https://www.channelnewsasia.com/'
+    reqs = requests.get(url)
+    soup = BeautifulSoup(reqs.text, 'html.parser')
+
+    linked_urls = set()
+
+    def get_all_links(url):
+        urls = set()
+        domain_name = urlparse(url).netloc
+        soup = BeautifulSoup(requests.get(url).content, "html.parser")
+        for a_tag in soup.findAll("a"):
+            href = a_tag.attrs.get("href")
+            if href == "" or href is None:
+                # href empty tag
+                continue
+            href = urljoin(url, href)
+            parsed_href = urlparse(href)
+            # remove URL GET parameters, URL fragments, etc.
+            href = parsed_href.scheme + "://" + parsed_href.netloc + parsed_href.path
+            if href in urls:
+                # already in the set
+                continue
+            if domain_name not in href:
+                # external link
+                continue
+            if len(href) > 60:
+                urls.add(href)
+        return urls
+    
+    urls_cna = get_all_links('https://www.channelnewsasia.com/')
+    url = random.sample(urls_cna, 1)
+    print("Found URL: ", url[0])
+    
+    page1 = requests.get(url[0])
+    soup1 = BeautifulSoup(page1.content, "html.parser")
+    text = list(soup1.find_all("p"))
+    cna_text = [txt.get_text() for txt in text]
+   
+    return cna_text
+
